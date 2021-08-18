115d041a3eba05fa04b05e96e468aa39e45bb8e7
takapamu <takapamukyary0129@gmail.com>
2021-08-18 20:00:55 +0900

    finish Janken

--- Janken.java
+++ Janken.java
@@ -1,5 +1,15 @@
 import java.util.Scanner;
 
-class Janken{
-  
+public class Janken{
+  public static void main(String[] args){
+
+    System.out.println("最初はグー! じゃんけんぽん!");
+
+    String[] cpuhands = {"グー", "チョキ", "パー"};
+
+    System.out.println("出したい番号を選んでください\n[0]グー\n[1]チョキ\n[2]パー");
+    Integer number = new Scanner(System.in).nextInt();
+
+    System.out.println("あなたの出した手: " + cpuhands[number]);
+  }
 }
