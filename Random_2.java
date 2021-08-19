507cb3d4cc86daab4bf62e517965056b8fad0631
takapamu <takapamukyary0129@gmail.com>
2021-08-19 20:00:43 +0900

    finish Random_2

---
+++ Random_2.java
@@ -0,0 +1,9 @@
+public class Random_2 {
+  public static void main(String[] args){
+    double d = Math.random();
+    System.out.println(d);
+
+    int i = (int)(Math.random() * 3);
+    System.out.println(i);
+  }
+}
